ffd1c5a5fff395ea3478e51aa61a2b11b20f8781
Bao Vu Hoang <baovuhbv@gmail.com>
2019-03-10 21:19:40 +0700

    Added copy-webpack-plugin

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -1,19 +1,20 @@
-const path = require('path');
-const merge = require('webpack-merge');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const webpackConfig = require('./webpack.config');
+const path = require("path");
+const merge = require("webpack-merge");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const webpackConfig = require("./webpack.config");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = merge(webpackConfig, {
+  devtool: "source-map",
 
-    devtool: 'source-map',
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].[chunkhash].js"
+  },
 
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: '[name].[chunkhash].js'
-    },
+  plugins: [
+    new CleanWebpackPlugin(["dist"]),
 
-    plugins: [
-        new CleanWebpackPlugin(['dist'])
-    ]
-
+    new CopyWebpackPlugin([{ from: "assets", to: "assets" }])
+  ]
 });
